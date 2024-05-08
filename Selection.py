100c0cd93c0fba5ec62ced65576463f603efb1a0
SiomaraCG <desireesiomara@gmail.com>
2024-05-08 14:35:02 -0500

    Ejercicio 3

--- Selection.py
+++ Selection.py
@@ -10,25 +10,29 @@ def selection_sort(arreglo):
         # Intercambiamos el elemento mínimo con el primer elemento restante
         arreglo[i], arreglo[min_index] = arreglo[min_index], arreglo[i]
 
-# Solicitar al usuario que ingrese los números separados por espacios
+# Define una función llamada obtener_numeros que solicita al usuario ingresar números separados por espacios
 def obtener_numeros():
+    # Lee la entrada del usuario y la almacena en input_str
     input_str = input("Ingrese los números separados por espacios: ")
     try:
+        # Intenta convertir los números ingresados en una lista de enteros
         numeros = [int(num) for num in input_str.split()]
     except ValueError:
+        # Si se produce un error de ValueError, muestra un mensaje de error y devuelve una lista vacía
         print("Error: Por favor, ingrese solo números separados por espacios.")
         return []
+    # Devuelve la lista de números ingresados por el usuario
     return numeros
 
-# Obtener la lista de números del usuario
+# Obtiene la lista de números del usuario llamando a la función obtener_numeros
 numeros = obtener_numeros()
 
-# Hacer una copia de la lista original para mantenerla intacta
+# Realiza una copia de la lista original para mantenerla intacta
 numeros_original = numeros.copy()
 
-# Ordenar la lista utilizando Selection Sort
+# Ordena la lista utilizando la función Selection Sort
 selection_sort(numeros)
 
-# Imprimir la lista original y la lista ordenada
+# Imprime la lista original y la lista ordenada
 print("Lista original:", numeros_original)
 print("Lista ordenada:", numeros)
